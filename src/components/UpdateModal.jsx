f3f0522d0f0380fe8c3321b01774d5b420e86c3f
Héctor <dreamallica@gmail.com>
2024-06-03 12:55:08 -0300

    Agregado de Formik

--- src/components/UpdateModal.jsx
+++ src/components/UpdateModal.jsx
@@ -1,20 +1,35 @@
 // src/components/UpdateModal.js
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const UpdateModal = ({ isOpen, onClose, onUpdate, clip }) => {
-    const [formData, setFormData] = useState({
-        title: '',
-        content: '',
-        priority: '',
-        type: '',
-        url: '',
-        released: '',
-        email: '',
+    const formik = useFormik({
+        initialValues: {
+            title: '',
+            content: '',
+            priority: '',
+            type: '',
+            url: '',
+            released: '',
+            email: '',
+        },
+        // Validaciones de los campos
+        validationSchema: Yup.object({
+            title: Yup.string().min(5, 'Debe contener al menos 5 caracteres').max(25, 'Debe contener menos de  25 caracteres').required('El título es requerido'),
+            content: Yup.string().min(10, 'Debe contener al menos 10 caracteres').max(150, 'Debe contener menos de  150 caracteres').required('El contenido es requerido'),
+            type: Yup.string().min(5, 'Debe contener al menos 5 caracteres').max(25, 'Debe contener menos de  25 caracteres').required('El tipo es requerido'),
+            url: Yup.string().max(80, 'Debe contener menos de  80 caracteres').url('Ingrese una URL válida').nullable(),
+        }),
+        onSubmit: (values) => {
+            onUpdate(clip.id, values);
+        },
     });
 
     useEffect(() => {
         if (clip) {
-            setFormData({
+            // Valores actuales de los campos del Form
+            formik.setValues({
                 title: clip.title,
                 content: clip.content,
                 priority: clip.priority,
@@ -26,25 +41,11 @@ const UpdateModal = ({ isOpen, onClose, onUpdate, clip }) => {
         }
     }, [clip]);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onUpdate(clip.id, formData);
-    };
-
     if (!isOpen) return null;
 
     return (
-        <div x-data="{ isOpen: true }" className="relative flex justify-center">
-            <button className="px-6 py-2 mx-auto tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
-                Open Modal
-            </button>
-
-            <div x-show="isOpen" className="fixed inset-0 z-10 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <div className="relative flex justify-center">
+            <div className="fixed inset-0 z-10 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
                 <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
                     <span className="hidden sm:inline-block sm:h-screen sm:align-middle" aria-hidden="true">
                         &#8203;
@@ -54,96 +55,93 @@ const UpdateModal = ({ isOpen, onClose, onUpdate, clip }) => {
                         <h3 className="text-lg font-medium leading-6 text-gray-800 capitalize dark:text-white" id="modal-title">
                             Edición de Clip
                         </h3>
-                        <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">Mofique los campos deseados y luego proceda a Actualizar.</p>
+                        <p className="mt-2 text-sm text-gray-500 dark:text-gray-400">Modifique los campos deseados y luego proceda a Actualizar.</p>
 
-                        <form className="mt-4" onSubmit={handleSubmit}>
-                            <label for="title" className="text-sm text-gray-700 dark:text-gray-200">
+                        <form className="mt-4" onSubmit={formik.handleSubmit}>
+                            <label htmlFor="title" className="text-sm text-gray-700 dark:text-gray-200">
                                 Título
                             </label>
+                            <input
+                                type="text"
+                                name="title"
+                                id="title"
+                                placeholder=""
+                                value={formik.values.title}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                className={`block w-full px-4 py-3 text-sm text-gray-700 bg-white border ${
+                                    formik.touched.title && formik.errors.title ? 'border-red-500' : 'border-gray-200'
+                                } rounded-md focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300`}
+                            />
+                            {formik.touched.title && formik.errors.title ? <div className="text-red-500 text-sm">{formik.errors.title}</div> : null}
 
-                            <label className="block mt-3" for="email">
-                                <input
-                                    type="text"
-                                    name="title"
-                                    id="title"
-                                    placeholder=""
-                                    value={formData.title}
-                                    onChange={handleChange}
-                                    className="block w-full px-4 py-3 text-sm text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"
-                                />
-                            </label>
-
-                            <label for="content" className="text-sm text-gray-700 dark:text-gray-200">
+                            <label htmlFor="content" className="text-sm text-gray-700 dark:text-gray-200 mt-3">
                                 Contenido
                             </label>
+                            <input
+                                type="text"
+                                name="content"
+                                id="content"
+                                placeholder=""
+                                value={formik.values.content}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                className={`block w-full px-4 py-3 text-sm text-gray-700 bg-white border ${
+                                    formik.touched.content && formik.errors.content ? 'border-red-500' : 'border-gray-200'
+                                } rounded-md focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300`}
+                            />
+                            {formik.touched.content && formik.errors.content ? <div className="text-red-500 text-sm">{formik.errors.content}</div> : null}
 
-                            <label className="block mt-3" for="email">
-                                <input
-                                    type="textarea"
-                                    name="content"
-                                    id="content"
-                                    placeholder=""
-                                    value={formData.content}
-                                    onChange={handleChange}
-                                    className="block w-full px-4 py-3 text-sm text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"
-                                />
-                            </label>
-
-                            <label for="priority" className="text-sm text-gray-700 dark:text-gray-200">
+                            <label htmlFor="priority" className="text-sm text-gray-700 dark:text-gray-200 mt-3">
                                 Prioridad
                             </label>
+                            <input
+                                type="text"
+                                name="priority"
+                                id="priority"
+                                placeholder=""
+                                value={formik.values.priority}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                className={`block w-full px-4 py-3 text-sm text-gray-700 bg-white border ${
+                                    formik.touched.priority && formik.errors.priority ? 'border-red-500' : 'border-gray-200'
+                                } rounded-md focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300`}
+                            />
+                            {formik.touched.priority && formik.errors.priority ? <div className="text-red-500 text-sm">{formik.errors.priority}</div> : null}
 
-                            <label className="block mt-3" for="email">
-                                <input
-                                    type="select"
-                                    name="priority"
-                                    id="priority"
-                                    placeholder=""
-                                    value={formData.priority}
-                                    onChange={handleChange}
-                                    className="block w-full px-4 py-3 text-sm text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"
-                                />
-                            </label>
-
-                            <label for="priority" className="text-sm text-gray-700 dark:text-gray-200">
+                            <label htmlFor="type" className="text-sm text-gray-700 dark:text-gray-200 mt-3">
                                 Tipo
                             </label>
+                            <input
+                                type="text"
+                                name="type"
+                                id="type"
+                                placeholder=""
+                                value={formik.values.type}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                className={`block w-full px-4 py-3 text-sm text-gray-700 bg-white border ${
+                                    formik.touched.type && formik.errors.type ? 'border-red-500' : 'border-gray-200'
+                                } rounded-md focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300`}
+                            />
+                            {formik.touched.type && formik.errors.type ? <div className="text-red-500 text-sm">{formik.errors.type}</div> : null}
 
-                            <label className="block mt-3" for="email">
-                                <input
-                                    type="text"
-                                    name="type"
-                                    id="type"
-                                    placeholder=""
-                                    value={formData.type}
-                                    onChange={handleChange}
-                                    className="block w-full px-4 py-3 text-sm text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"
-                                />
-                            </label>
-
-                            <label for="priority" className="text-sm text-gray-700 dark:text-gray-200">
+                            <label htmlFor="url" className="text-sm text-gray-700 dark:text-gray-200 mt-3">
                                 URL
                             </label>
-
-                            <label className="block mt-3" for="email">
-                                <input
-                                    type="text"
-                                    name="url"
-                                    id="url"
-                                    placeholder=""
-                                    value={formData.url}
-                                    onChange={handleChange}
-                                    className="block w-full px-4 py-3 text-sm text-gray-700 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"
-                                />
-                            </label>
-
-                            <button type="button" className="mt-2 flex items-center rounded py-1.5 px-2 text-sm text-blue-600 transition-colors duration-300 hover:text-blue-400 focus:outline-none dark:text-blue-400 dark:hover:text-blue-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                                </svg>
-
-                                <span className="mx-2">Agregar campo</span>
-                            </button>
+                            <input
+                                type="text"
+                                name="url"
+                                id="url"
+                                placeholder=""
+                                value={formik.values.url}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                className={`block w-full px-4 py-3 text-sm text-gray-700 bg-white border ${
+                                    formik.touched.url && formik.errors.url ? 'border-red-500' : 'border-gray-200'
+                                } rounded-md focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300`}
+                            />
+                            {formik.touched.url && formik.errors.url ? <div className="text-red-500 text-sm">{formik.errors.url}</div> : null}
 
                             <div className="mt-4 sm:flex sm:items-center sm:-mx-2">
                                 <button
@@ -155,7 +153,7 @@ const UpdateModal = ({ isOpen, onClose, onUpdate, clip }) => {
                                 </button>
 
                                 <button
-                                    ttype="submit"
+                                    type="submit"
                                     className="w-full px-4 py-2 mt-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md sm:mt-0 sm:w-1/2 sm:mx-2 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
                                 >
                                     Actualizar
